0254dae38a6588cb11bfda1e60685aef5a95e7c1
Matt Sullivan <1115325+matt-sullivan@users.noreply.github.com>
2026-03-04 02:19:45 +1000

    Update toothbrush display mode logic to allow peeking at stats

--- esphome/toothbrush_days_to_show.h
+++ esphome/toothbrush_days_to_show.h
@@ -7,7 +7,14 @@ struct DayToShow {
   float pm;
 };
 
-typedef enum {Mode_Welcome, Mode_Brushing, Mode_Stats, Mode_Off} DisplayMode;
+typedef enum {
+  Mode_Init,       // after reboot, before sensor values received
+  Mode_Welcome,    // idle "Hello Toby" screen
+  Mode_Brushing,   // active brushing display
+  Mode_StatsPeek,  // temporary stats peek (5 s, then returns to peek_return_mode)
+  Mode_Stats,      // post-brush stats (60 s, then Off)
+  Mode_Off,        // screen off
+} DisplayMode;
 
 const int TargetDuration = 120;
 
